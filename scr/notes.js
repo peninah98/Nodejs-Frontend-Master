33ec641bfbf955df89065e09d6b3a38cf91302e4
peninah98 <peninangizwenayo@gmail.com>
2024-02-18 10:29:46 +0200

    feat: creating the note file

--- scr/notes.js
+++ scr/notes.js
@@ -1,24 +1,37 @@
-import { getDB, insertDB, saveDB } from "./db"
+import { insert, getDB, saveDB } from "./db.js"
 
 export const newNote = async (note, tags) => {
-  const newNote = {
+  const data = {
     tags,
-    id: Date.now(),
     content: note,
+    id: Date.now(),
   }
-  await insertDB(newNote)
-  return newNote
+  await insert(data)
+  return data
 }
-
 export const getAllNotes = async () => {
-  const { notes } = await getDB()
-  return notes
+  const db = await getDB()
+  return db.notes
 }
 
-
-export const findNotes = async (filter)=>{
-const {notes}=await getDB();
-return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()))
+export const findNotes = async (filter) => {
+  const notes = await getAllNotes()
+  return notes.filter((note) =>
+    note.content.toLowerCase().includes(filter.toLowerCase())
+  )
 }
 
-  
+export const removeNote = async (id) => {
+  const notes = await getAllNotes()
+  const match = notes.find((note) => note.id === id)
+
+  if (match) {
+    const newNotes = notes.filter((note) => note.id !== id)
+    await saveDB({ notes: newNotes })
+    return id
+  }
+}
+
+export const removeAllNotes = async () => {
+  await saveDB({ notes: [] })
+}
